ff6298ecbe67c589d22dffad5c09d87861808bff
= <=>
2019-10-29 12:26:23 +0700

    first commit

---
+++ app/setup.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+
+import store from './store';
+import Main from './index';
+
+function setup() {
+  class Root extends Component {
+    render() {
+      return (
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      );
+    }
+  }
+
+  return Root;
+}
+
+module.exports = setup;
